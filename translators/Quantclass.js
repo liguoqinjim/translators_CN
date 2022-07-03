433e1b849ffbe40d3c6b03e7c6d72746cb05ec74
liguoqinjim <liguoqinjim23@gmail.com>
2022-07-03 21:13:33 +0800

    feat($): update

--- translators/Quantclass.js
+++ translators/Quantclass.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2022-06-29 15:42:11"
+	"lastUpdated": "2022-07-02 03:45:54"
 }
 
 /*
@@ -37,36 +37,11 @@
 
 
 async function detectWeb(doc, url) {
-	// TODO: adjust the logic here
-	// if (url.includes('/article/')) {
-	// 	return 'newspaperArticle';
-	// }
-	// else if (getSearchResults(doc, true)) {
-	// 	return 'multiple';
-	// }
 	if (url.includes("/thread")){
 		return "webpage"
 	}
 
 	return false;
-}
-
-function getSearchResults(doc, checkOnly) {
-	var items = {};
-	var found = false;
-	// TODO: adjust the CSS selector
-	var rows = doc.querySelectorAll('h2 > a.title[href*="/article/"]');
-	for (let row of rows) {
-		// TODO: check and maybe adjust
-		let href = row.href;
-		// TODO: check and maybe adjust
-		let title = ZU.trimInternal(row.textContent);
-		if (!href || !title) continue;
-		if (checkOnly) return true;
-		found = true;
-		items[href] = title;
-	}
-	return found ? items : false;
 }
 
 async function doWeb(doc, url) {
@@ -109,7 +84,7 @@ function scrape(doc, url = doc.location.href) {
 	item.attachments.push({"url": url, title: "Snapshot"});
 	item.sector = sector;
 	// item.postType = sector;
-	
+
 	item.complete();
 }
 
